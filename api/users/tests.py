31465ada8115fad725d67d6f5ae01622612adad0
gwangyonglee <gwangyonglee92@gmail.com>
2019-05-08 00:23:20 +0900

    유저 생성 테스트 케이스 작성

--- api/users/tests.py
+++ api/users/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status
+from .models import User
 
-# Create your tests here.
+class UserTest(APITestCase):
+    def test_create_user_success(self):
+        test_case = [
+            {
+                "username" : "gwangd",
+                "email"    : "gwangyonglee92@gmail.com",
+                "password" : "qwe123"
+            }
+        ]
+        for i in test_case:
+            res = self.client.post('/api/v1/users/', data=i)
+            self.assertEquals(res.status_code, status.HTTP_201_CREATED)
